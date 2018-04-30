2127a74c657181993b242864c1c532a2c8c84c9d
theogenes <theo.duarte@icloud.com>
2018-04-30 11:53:14 -0300

    ajuste busca odonto

--- app/Http/Controllers/AtendimentoController.php
+++ app/Http/Controllers/AtendimentoController.php
@@ -40,52 +40,36 @@ class AtendimentoController extends Controller
         $atendimentos = [];
         $list_atendimentos = [];
         $list_enderecos = [];
+        $locais_google_maps = [];
         
         $endereco = Endereco::findorfail($endereco_id);
         $endereco->load('cidade');
         $ct_atendimento = Atendimento::findorfail($atendimento_id);
         $cidade_id = $endereco->cidade->id;
         
-        if ($tipo_atendimento == 'saude') {
+        if ($tipo_atendimento == 'saude' | $tipo_atendimento == 'odonto') {
+            
+            $tipo_atendimento_id = $tipo_atendimento == 'saude' ? 1 : 2;
         	 
+            //-- realiza a lista dos atendimentos do tipo CONSULTA MEDICA---------------
         	//DB::enableQueryLog();
         	$atendimentos = Atendimento::with('clinica')
-        	    ->join('consultas', function($join1) { $join1->on('consultas.id', '=', 'atendimentos.consulta_id')->where('consultas.tipoatendimento_id', '=', 1);})
-        		->join('clinicas', function($join2) { $join2->on('clinicas.id', '=', 'atendimentos.clinica_id');})
-        		->join('clinica_endereco', function($join3) { $join3->on('clinicas.id', '=', 'clinica_endereco.clinica_id');})
-        		->join('enderecos', function($join4) use ($endereco_id) { $join4->on('clinica_endereco.endereco_id', '=', 'enderecos.id')->on('enderecos.id', '=', DB::raw($endereco_id));})
-        		->where('atendimentos.consulta_id', '=', $ct_atendimento->consulta_id)
-        		->orderBy('atendimentos.vl_com_atendimento', $sort_item)
-        		->select('atendimentos.*', 'atendimentos.id', 'atendimentos.vl_com_atendimento', 'atendimentos.vl_net_atendimento', 'atendimentos.ds_preco', 'atendimentos.consulta_id')
-        		->distinct()
-        		->get();
-        	 
-        	//$especialidades = Especialidade::orderBy('ds_especialidade', 'asc')->pluck('ds_especialidade', 'id');
+        	   ->join('consultas', function($join1) use ($tipo_atendimento_id) { $join1->on('consultas.id', '=', 'atendimentos.consulta_id')->where('consultas.tipoatendimento_id', '=', DB::raw($tipo_atendimento_id));})
+        	   ->join('clinicas', function($join2) { $join2->on('clinicas.id', '=', 'atendimentos.clinica_id');})
+        	   ->join('clinica_endereco', function($join3) { $join3->on('clinicas.id', '=', 'clinica_endereco.clinica_id');})
+        	   ->join('enderecos', function($join4) use ($endereco_id) { $join4->on('clinica_endereco.endereco_id', '=', 'enderecos.id')->on('enderecos.id', '=', DB::raw($endereco_id));})
+        	   ->where('atendimentos.consulta_id', '=', $ct_atendimento->consulta_id)
+        	   ->orderBy('atendimentos.vl_com_atendimento', $sort_item)
+        	   ->select('atendimentos.*', 'atendimentos.id', 'atendimentos.vl_com_atendimento', 'atendimentos.vl_net_atendimento', 'atendimentos.ds_preco', 'atendimentos.consulta_id')
+        	   ->distinct()
+        	   ->get();
+        	
         	//$query = DB::getQueryLog();
         	//print_r($query);
-        	$locais_google_maps = [];
-//         	var clinicaTres = {
-//         	    info: '<strong>Clínica Devas</strong><br>\r\
-//                                 SDN CNB Etapa III - S 4104, Setor de<br> Diversões Norte - Brasília, DF, 70077-000<br>\
-//                                 <a href="https://goo.gl/2JdPbn">Obter direção</a>',
-//                                 lat: -15.7920841,
-//                                 long: -47.8859702
-//         	};
         	
-        	foreach ($atendimentos as $atend) {
-        	    $atend->clinica->load('enderecos');
-        	    $atend->clinica->enderecos->first()->load('cidade');
-        	    $info = "<strong>".$atend->clinica->nm_fantasia."</strong><br> ".$atend->clinica->enderecos->first()->te_endereco."<br> ".$atend->clinica->enderecos->first()->te_bairro." - ".$atend->clinica->enderecos->first()->cidade->nm_cidade.", ".$atend->clinica->enderecos->first()->cidade->sg_estado.", ".$atend->clinica->enderecos->first()->getCepFormatado()."<br>";
-        	    $lat = $atend->clinica->enderecos->first()->nr_latitude_gps;
-        	    $long = $atend->clinica->enderecos->first()->nr_longitute_gps;
-        	    
-        	    $item_google_maps = ['info' => $info, 'lat' => $lat, 'long' => $long];
-        	    
-        	    array_push($locais_google_maps, $item_google_maps);
-        	}
-        	
+        	//-- seleciona os itens de atendimento removendo as repeticoes-----------
         	$atendimentos_temp = DB::table('atendimentos')
-        	   ->join('consultas', function($join1) { $join1->on('consultas.id', '=', 'atendimentos.consulta_id')->where('consultas.tipoatendimento_id', '=', 1);})
+        	   ->join('consultas', function($join1) use ($tipo_atendimento_id) { $join1->on('consultas.id', '=', 'atendimentos.consulta_id')->where('consultas.tipoatendimento_id', '=', DB::raw($tipo_atendimento_id));})
         	   ->select('atendimentos.*')
         	   ->distinct()
         	   ->get(['consultas.cd_consulta']);
@@ -105,8 +89,10 @@ class AtendimentoController extends Controller
     		    }
     		}
     		
+    		//-- seleciona o bairro e converte para o formato de search_term -------
     		$local_atendimento = UtilController::toStr($endereco->te_bairro);
     		
+    		//-- busca dos enderecos disponíveis de atendimento --------------------
     		$consulta_id = $ct_atendimento->consulta_id;
     		$enderecos = Endereco::with('cidade')
     		    ->join('cidades', function($join1) use ($local_atendimento) { $join1->on('cidades.id', '=', 'enderecos.cidade_id')->on(DB::raw('to_str(cidades.nm_cidade)'), 'LIKE', DB::raw("'%".$local_atendimento."%'"))->orOn(DB::raw('to_str(enderecos.te_endereco)'), 'LIKE', DB::raw("'%".$local_atendimento."%'"))->orOn(DB::raw('to_str(enderecos.te_bairro)'), 'LIKE', DB::raw("'%".$local_atendimento."%'"));})
@@ -120,8 +106,10 @@ class AtendimentoController extends Controller
         		->distinct()
         		->get();
         	
-        	$list_endereco_ids = []; 
+        	//-- lista enderecos id usada para aplicar clausula NOI IN na lista dos demais enderecos ---
+        	$list_endereco_ids = [];
         	
+        	//-- realiza a conversao dos itens para exibicao no droplist da landing page ---------------
         	foreach ($enderecos as $query)
         	{
         	    $arResultado = [ 'id' =>  $query->id, 'cidade_id' => $query->cidade_id, 'value' => $query->te_bairro.': '.$query->cidade->nm_cidade ];
@@ -129,6 +117,7 @@ class AtendimentoController extends Controller
         	    array_push($list_endereco_ids, $query->id);
             }
         	
+            //-- busca os demais enderecos disponíveis de atendimento --------------------
     		$outros_enderecos = Endereco::with('cidade')
     		    ->join('cidades', function($join1) use ($cidade_id) { $join1->on('cidades.id', '=', 'enderecos.cidade_id')->on('cidades.id', '=', DB::raw($cidade_id));})
         		->join('clinica_endereco', function($join2) { $join2->on('enderecos.id', '=', 'clinica_endereco.endereco_id');})
@@ -140,15 +129,29 @@ class AtendimentoController extends Controller
         		->select('enderecos.*', 'enderecos.id', 'enderecos.te_endereco', 'enderecos.te_bairro', 'enderecos.cidade_id')
         		->distinct()
         		->get();
-        	
-//         	dd($outros_enderecos);
     		
+        	//-- realiza a conversao dos itens para exibicao no droplist da landing page ---------------
         	foreach ($outros_enderecos as $query)
     		{
     		    $arResultado = [ 'id' =>  $query->id, 'cidade_id' => $query->cidade_id, 'value' => $query->te_bairro.': '.$query->cidade->nm_cidade ];
     		    array_push($list_enderecos, $arResultado);
     		}
         
+        } elseif ($tipo_atendimento == 'exame') {
+            
+        }
+        
+        //-- realiza a lista dos locais para uso do GOOGLE MAPS---------------
+        foreach ($atendimentos as $atend) {
+            $atend->clinica->load('enderecos');
+            $atend->clinica->enderecos->first()->load('cidade');
+            $info = "<strong>".$atend->clinica->nm_fantasia."</strong><br> ".$atend->clinica->enderecos->first()->te_endereco."<br> ".$atend->clinica->enderecos->first()->te_bairro." - ".$atend->clinica->enderecos->first()->cidade->nm_cidade.", ".$atend->clinica->enderecos->first()->cidade->sg_estado.", ".$atend->clinica->enderecos->first()->getCepFormatado()."<br>";
+            $lat = $atend->clinica->enderecos->first()->nr_latitude_gps;
+            $long = $atend->clinica->enderecos->first()->nr_longitute_gps;
+            
+            $item_google_maps = ['info' => $info, 'lat' => $lat, 'long' => $long];
+            
+            array_push($locais_google_maps, $item_google_maps);
         }
         
         return view('resultado', compact('atendimentos', 'list_atendimentos', 'list_enderecos', 'tipo_atendimento', 'locais_google_maps'));
