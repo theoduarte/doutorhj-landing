a5f352d8727f3156b2feb9546ead1a3a6e4e198e
theoduarte <theo@combotec.com.br>
2018-05-27 18:15:01 -0300

    ajustes

--- app/Http/ViewComposer/MenuComposer.php
+++ app/Http/ViewComposer/MenuComposer.php
@@ -31,35 +31,41 @@ class MenuComposer
 	    $cvx_num_itens_carrinho = $cartCollection->count();
 	    
 	    //--busca as notificacoes do paciente-----------------------
-	    $notificacoes_app = Mensagem::with('remetente')
-    	    ->join('mensagem_destinatarios', function($join1) { $join1->on('mensagem_destinatarios.mensagem_id', '=', 'mensagems.id');})
-    	    ->where(function ($query) use ($user_session) { $query->where('mensagem_destinatarios.destinatario_id', $user_session->id);})
-    	    ->where(DB::raw('mensagem_destinatarios.cs_status'), '=', 'A')
-    	    ->where(DB::raw('mensagem_destinatarios.visualizado'), '=', 'false')
-    	    ->orderBy('mensagem_destinatarios.created_at', 'desc')
-    	    ->limit(3)
-    	    ->get();
-    	
-	    for ($i=0; $i < sizeof($notificacoes_app); $i++) {
-	        $nome_remetente = '';
+	    $notificacoes_app = [];
+	    $num_total_notificacoes = 0;
+	    
+	    if (isset($user_session)) {
 	        
-	        $nome_remetente = UtilController::getBetween($notificacoes_app[$i]->conteudo, '<li>Nome:', '</li>');
+	        $notificacoes_app = Mensagem::with('remetente')
+	        ->join('mensagem_destinatarios', function($join1) { $join1->on('mensagem_destinatarios.mensagem_id', '=', 'mensagems.id');})
+	        ->where(function ($query) use ($user_session) { $query->where('mensagem_destinatarios.destinatario_id', $user_session->id);})
+	        ->where(DB::raw('mensagem_destinatarios.cs_status'), '=', 'A')
+	        ->where(DB::raw('mensagem_destinatarios.visualizado'), '=', 'false')
+	        ->orderBy('mensagem_destinatarios.created_at', 'desc')
+	        ->limit(3)
+	        ->get();
 	        
-	        if($nome_remetente == '') {
-	            $nome_remetente = str_replace("Contato", "", $notificacoes_app[$i]->rma_nome);
+	        for ($i=0; $i < sizeof($notificacoes_app); $i++) {
+	            $nome_remetente = '';
+	            
+	            $nome_remetente = UtilController::getBetween($notificacoes_app[$i]->conteudo, '<li>Nome:', '</li>');
+	            
+	            if($nome_remetente == '') {
+	                $nome_remetente = str_replace("Contato", "", $notificacoes_app[$i]->rma_nome);
+	            }
+	            
+	            $notificacoes_app[$i]->nome_remetente = trim($nome_remetente);
+	            $notificacoes_app[$i]->time_ago = UtilController::timeAgo(date('Y-m-d H:i:s', strtotime($notificacoes_app[$i]->getRawCreatedAtAttribute())));
 	        }
 	        
-	        $notificacoes_app[$i]->nome_remetente = trim($nome_remetente);
-	        $notificacoes_app[$i]->time_ago = UtilController::timeAgo(date('Y-m-d H:i:s', strtotime($notificacoes_app[$i]->getRawCreatedAtAttribute())));
+	        $total_notificacoes = Mensagem::with('remetente')
+	        ->join('mensagem_destinatarios', function($join1) { $join1->on('mensagem_destinatarios.mensagem_id', '=', 'mensagems.id');})
+	        ->where(function ($query) use ($user_session) { $query->where('mensagem_destinatarios.destinatario_id', $user_session->id);})->where(DB::raw('mensagem_destinatarios.cs_status'), '=', 'A')
+	        ->orderBy('mensagem_destinatarios.updated_at', 'desc')
+	        ->get();
+	        
+	        $num_total_notificacoes = sizeof($total_notificacoes);
 	    }
-	    
-	    $total_notificacoes = Mensagem::with('remetente')
-    	    ->join('mensagem_destinatarios', function($join1) { $join1->on('mensagem_destinatarios.mensagem_id', '=', 'mensagems.id');})
-    	    ->where(function ($query) use ($user_session) { $query->where('mensagem_destinatarios.destinatario_id', $user_session->id);})->where(DB::raw('mensagem_destinatarios.cs_status'), '=', 'A')
-    	    ->orderBy('mensagem_destinatarios.updated_at', 'desc')
-    	    ->get();
-	    
-	    $num_total_notificacoes = sizeof($total_notificacoes);
 	    
 
 	    $view->with('menus_app', $menus_app);
