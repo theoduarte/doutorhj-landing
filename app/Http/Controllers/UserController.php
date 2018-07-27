971135bfd4148efd864d4809383e2b8e5a2b1e94
EGISBERTO V SILVA <egisbertovsilva@Mac-mini-de-EGISBERTO.local>
2018-07-27 16:55:41 -0300

    Change the contact email to cliente@doctorhoje.com.br

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -308,7 +308,7 @@ class UserController extends Controller
                     Em caso de qualquer dúvida, fique à vontade <br>
                     para responder esse e-mail ou
                     nos contatar no <br><br>
-                    <a href='mailto:meajuda@doctorhoje.com.br' style='color:#1d70b7; text-decoration: none;'>meajuda@doctorhoje.com.br</a>
+                    <a href='mailto:cliente@doctorhoje.com.br' style='color:#1d70b7; text-decoration: none;'>cliente@doctorhoje.com.br</a>
                     <br><br>
                     Ou ligue para (61) 3221-5350, o atendimento é de<br>
                     segunda à sexta-feira
@@ -472,7 +472,7 @@ HEREDOC;
                     Em caso de qualquer dúvida, fique à vontade <br>
                     para responder esse e-mail ou
                     nos contatar no <br><br>
-                    <a href='mailto:meajuda@doctorhoje.com.br' style='color:#1d70b7; text-decoration: none;'>meajuda@doctorhoje.com.br</a>
+                    <a href='mailto:cliente@doctorhoje.com.br' style='color:#1d70b7; text-decoration: none;'>cliente@doctorhoje.com.br</a>
                     <br><br>
                     Ou ligue para (61) 3221-5350, o atendimento é de<br>
                     segunda à sexta-feira
