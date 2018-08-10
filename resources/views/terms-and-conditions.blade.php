f725811b9b5216557bd58dbf299561040cb8ef16
EGISBERTO V SILVA <egisbertovsilva@Mac-mini-de-EGISBERTO.local>
2018-08-10 10:38:50 -0300

    Add new url for terms and conditions

---
+++ resources/views/terms-and-conditions.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.raw')
+
+@section('title', 'DoctorHoje: Termos e Condições de Uso e Política de Segurança e Privacidade')
+
+@section('content')
+<div class="modal-termos container">
+    {!! $termosCondicoesActual !!}
+</div>
+@endsection
