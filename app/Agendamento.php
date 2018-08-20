cf6339921078f0fff6710ec9ef51e16fed45dfd8
EGISBERTO V SILVA <egisbertovsilva@Mac-mini-de-EGISBERTO.local>
2018-08-20 08:43:21 -0300

    Add the faturado and pago status

--- app/Agendamento.php
+++ app/Agendamento.php
@@ -26,6 +26,8 @@ class Agendamento extends Model
     const CANCELADO      = 60;
     const AGENDADO       = 70;
     const RETORNO        = 80;
+    const FATURADO       = 90;
+    const PAGO           = 100;
     
     protected static $cs_status = array(
         self::PRE_AGENDADO   => 'Pré-Agendado',
@@ -35,10 +37,10 @@ class Agendamento extends Model
         self::AUSENTE        => 'Ausente',
         self::CANCELADO      => 'Cancelado',
         self::AGENDADO       => 'Agendado',
-        self::RETORNO        => 'Retorno'
+        self::RETORNO        => 'Retorno',
+        self::FATURADO       => 'Faturado',
+        self::PAGO           => 'Pago'
     );
-    
-    
     
     /*
      * Relationship
