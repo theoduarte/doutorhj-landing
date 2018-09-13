84ecfeaeef132784deada5c8f079f481ff0d5115
Paulo Kenobi <kenobipaulo@gmail.com>
2018-09-13 11:55:21 -0300

    Adicionado novas opções no menu do usuário

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -110,12 +110,29 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('minha-conta') }}">Minha Conta</a>
                             </li>
+                            <li class="nav-item dropdown menu-usuario">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="al-nome-usuario">{{ Auth::user()->paciente->nm_primario }}</span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <div class="opcoes">
+                                        <p class="titulo">Plano</p>
+                                        <p class="plano premium">Premium</p>
+                                        {{--<p class="plano blue">Blue</p>
+                                        <p class="plano black">Black</p>--}}
+                                    </div>
+                                    <div class="dropdown-divider"></div>
+                                    <div class="opcoes">
+                                        <p class="titulo">Saldo</p>
+                                        <p class="saldo">R$ 300</p>
+                                    </div>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#v-pill-sair">Sair</a>
+                                </div>
+                            </li>
                         </ul>
                         <div class="menu-area-logada">
                             <ul>
-                                <li>
-                                    <span class="al-nome-usuario">{{ Auth::user()->paciente->nm_primario }}</span>
-                                </li>
                                 <li>
                                     <div class="dropdown opcoes-menu-usuario drop-notificacoes">
                                         <!-- <button class="btn dropdown-toggle btn-notificacoes btn-area-logada" title="Notificações" type="button" id="dropdownNotificacoes" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
