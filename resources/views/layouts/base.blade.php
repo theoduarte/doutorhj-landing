8a11053243224b1e2803053a8dc35774583524f1
theoduarte <theo@combotec.com.br>
2018-11-10 12:06:34 -0200

    ajustes

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -519,7 +519,7 @@
 
         $(document).ready(function () {
 
-        	docCookies.removeItem('uf_localizacao');
+        	//docCookies.removeItem('uf_localizacao');
         	showModalUfLocation();
 
         });
