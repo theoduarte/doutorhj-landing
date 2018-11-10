fde87e7afe05f446ebe0c2fe0af9e49383b4bc31
theoduarte <theo@combotec.com.br>
2018-11-10 10:50:45 -0200

    ajustes

--- public/js/doutorhj.geolocation.js
+++ public/js/doutorhj.geolocation.js
@@ -1,13 +1,15 @@
 window.onload = function() {
 	var startPos;
-	  var geoOptions = {
-	     timeout: 30 * 1000
-	  }
+	var drhj_latitude = 0;
+	var drhj_longitude = 0;
+	var geoOptions = {
+		timeout: 30 * 1000
+	}
 
 	  var geoSuccess = function(position) {
-	    startPos = position;
-	    document.getElementById('startLat').value = startPos.coords.latitude;
-	    document.getElementById('startLon').value = startPos.coords.longitude;
+	    startPos 	   = position;
+	    user_latitude  = startPos.coords.latitude;
+	    user_longitude = startPos.coords.longitude;
 	  };
 	  var geoError = function(error) {
 	    console.log('Error occurred. Error code: ' + error.code);
@@ -16,6 +18,21 @@ window.onload = function() {
 	    //   1: permission denied
 	    //   2: position unavailable (error response from location provider)
 	    //   3: timed out
+	    if(error.code == 0) {
+	    	
+	    	var drhj_googlemaps_key = 'AIzaSyCkovLYQa6lqh1suWtV_ZFJ0i9ChWc9hqI';
+	    	jQuery.ajax({
+	    		type: 'GET',
+	    	  	url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+drhj_latitude+','+drhj_longitude+'&key='+drhj_googlemaps_key,
+	    	  	data: {},
+				success: function (result) {
+					codeLatLng(result);
+	            },
+	            error: function (result) {
+	            	$.Notification.notify('error','top right', 'DrHoje', 'Falha no carregamento da sua localização!');
+	            }
+	    	});
+	    }
 	    if(error.code == 1 | error.code == 3) {
 	    	showModalUfLocation();
 	    }
@@ -58,4 +75,32 @@ function showModalUfLocation() {
         docCookies.setItem('uf_localizacao', sg_estado);
         $('#sg_estado_localizazao_form').val(sg_estado);
     });
+}
+
+function codeLatLng(results) {
+	
+	console.log(results)
+    if (results[1]) {
+      //formatted address
+      alert(results[0].formatted_address)
+      //find country name
+      for (var i=0; i<results[0].address_components.length; i++) {
+          for (var b=0; b<results[0].address_components[i].types.length;b++) {
+
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == "administrative_area_level_1") {
+                  //this is the object you are looking for
+                  city= results[0].address_components[i];
+                  break;
+              }
+          }
+      }
+      //city data
+      alert(city.short_name + " " + city.long_name)
+
+
+      } else {
+        alert("No results found");
+      }
+}
 }
