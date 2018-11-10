822aa63d453f646f2ff9cfaab1d570baa80d2f30
theoduarte <theo@combotec.com.br>
2018-11-10 11:57:26 -0200

    ajustes

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -520,39 +520,7 @@
         $(document).ready(function () {
 
         	//docCookies.removeItem('uf_localizacao');
-
-            /* $('.seleciona-estado').select2({
-                width: '100%',
-                language: {
-                    noResults: function (params) {
-                      return "Nenhum Estado encontrado!";
-                    }
-                  }
-            });
-
-            var uf_localizacao = docCookies.getItem('uf_localizacao');
-            
-            $('#sg_estado_localizacao').val(uf_localizacao);
-            $('#sg_estado_localizazao_form').val(uf_localizacao);
-
-            //alert(uf_localizacao);
-            if(uf_localizacao === null) {
-                
-            	$('#modalEstado').modal({
-                    backdrop: 'static',
-                    keyboard: false,
-                });
-                
-            } else {
-            	var ds_uf_localizacao = $('#sg_estado_localizacao').select2('data')[0].text;
-                $('#ds_uf_localizacao').html(ds_uf_localizacao);
-            }
-
-            $('#sg_estado_localizacao').change(function(){
-                var sg_estado = $(this).val();
-                docCookies.setItem('uf_localizacao', sg_estado);
-                $('#sg_estado_localizazao_form').val(sg_estado);
-            }); */
+        	showModalUfLocation();
 
         });
 
