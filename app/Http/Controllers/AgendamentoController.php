de39604a92470b797b1844bfa2b9b076af1a6378
Hugo Richard <hugorichard2010@gmail.com>
2018-11-12 14:13:07 -0200

    modificou

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 use App\Agendamento;
 use App\Clinica;
+use App\User;
 use App\Profissional;
 use App\Estado;
 use App\Atendimento;
