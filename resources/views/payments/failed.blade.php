6f7a4fdf2af65cb89a8a948813f9b3d50f3cb0b0
Hugo Richard <hugorichard2010@gmail.com>
2018-11-27 16:07:20 -0200

    telas de sucesso e falha apos o pagamento finalizadas

--- resources/views/payments/failed.blade.php
+++ resources/views/payments/failed.blade.php
@@ -3,6 +3,11 @@
 @push('scripts')
 @endpush
 @section('content')
-
-
+    <div class="container">
+        <div class="status-pagamento-boleto spb-erro">
+            <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
+            <h3>Ocorreu um erro no pagamento!</h3>
+            <p>Por favor, tente novamente.</p>
+        </div>
+    </div>
 @endsection
--- resources/views/payments/success.blade.php
+++ resources/views/payments/success.blade.php
@@ -3,5 +3,11 @@
 @push('scripts')
 @endpush
 @section('content')
-
+    <div class="container">
+        <div class="status-pagamento-boleto spb-sucesso">
+            <i class="fa fa-check-circle" aria-hidden="true"></i>
+            <h3>Pagamento realizado com sucesso!</h3>
+            <p>Obrigado por escolher o Doutor Hoje.</p>
+        </div>
+    </div>
 @endsection
