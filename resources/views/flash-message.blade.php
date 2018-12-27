d6d3c9214bc7a4bbb30584c94836e4d295e3c6f9
Vitor Pagani <vitor.pagani.92@gmail.com>
2018-12-27 11:49:28 -0200

    Adicionado código para limpar o caché, localstorage quando ocorrer erro.

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -99,6 +99,26 @@ $(document).ready(function () {
 </script>
 @endif
 
+@if ($message = Session::get('erro-clear-storage'))
+<script type="text/javascript">
+$(document).ready(function () {
+	localStorage.clear();
+	sessionStorage.clear();
+	var cookies = document.cookie;
+
+	for (var i = 0; i < cookies.split(";").length; ++i)
+	{
+		var myCookie = cookies[i];
+		var pos = myCookie.indexOf("=");
+		var name = pos > -1 ? myCookie.substr(0, pos) : myCookie;
+		document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+	}
+
+	$.Notification.notify('error','top right', 'Solicitação Falhou!', '{{ $message }}');
+});
+</script>
+@endif
+
 @if ($errors->any())
 <script type="text/javascript">
 $(document).ready(function () {
