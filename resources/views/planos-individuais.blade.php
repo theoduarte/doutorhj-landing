b434fa69d1f4fbcffc05aac3543d8e1119056ff3
Paulo Kenobi <kenobipaulo@gmail.com>
2019-01-17 08:54:52 -0200

    Ajustes LP PF

--- resources/views/planos-individuais.blade.php
+++ resources/views/planos-individuais.blade.php
@@ -6,59 +6,142 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link type="text/css" rel="stylesheet" href="/libs/home-template/css/bootstrap.min.css"/>
     <link type="text/css" rel="stylesheet" href="/libs/font-awesome-4.7.0/css/font-awesome.min.css"/>
+    <link type="text/css" rel="stylesheet" href="/libs/home-template/css/style.css"/>
 
     <title>Planos Individuais - Doutor Hoje</title>
 </head>
 <body>
-<header>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Planos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Vantagens</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Perguntas Frequentes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Depoimentos</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <a href="#" class="btn btn-primary">Assine Agora</a>
-                </form>
-            </div>
-        </nav>
-    </div>
-</header>
-<section>
-    <div class="banner">
+<div class="lp-pessoa-fisica">
+    <header>
         <div class="container">
-            <div class="area-texto">
-                <div class="fundo-textura"></div>
-                <div class="texto">
-                    <p class="titulo"><strong>Quer viver melhor?</strong><br>
-                        se prevenir é o segredo!</p>
-                    <p>Com o Doutor Hoje você pode se manter em dia com sua saúde por um preço justo e ainda melhorar
-                        sua qualidade de vida para aproveitar o que a vida oferece de melhor.</p>
-                    <div class="valor">
-                        <p>Consultas a partir de:</p>
-                        <p><strong>R$ 29,90</strong></p>
+            <nav class="navbar navbar-expand-lg">
+                <a class="navbar-brand" href="#"><img src="/libs/home-template/img/logo-padrao.png" alt="Doutor Hoje"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Planos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Vantagens</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Perguntas Frequentes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Depoimentos</a>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <a href="#" class="btn">Assine Agora</a>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <section>
+        <div class="faixa">
+            <div class="container">
+                <p>Corporativo</p>
+            </div>
+        </div>
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 apresentacao">
+                        <p>O Doutor Hoje é uma rede que oferece </p>
+                        <h1>consultas exames a<br><strong>preços justos.</strong></h1>
+                        <img src="/libs/home-template/img/l-pf-cartoes.png" alt="">
+                    </div>
+                    <div class="col-sm-12 col-md-6 imagem">
+                        <img src="/libs/home-template/img/l-pf-banner-topo.png" alt="Menina no colo da mãe, sorrindo, sendo atendida por um médico">
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+        <div class="tabela-planos">
+            <div class="container">
+                <div class="area-tabela">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th scope="col">
+                                <p>Assinando os planos Blue ou Black você pode ter acesso a consultas médicas a partir de <strong>R$ 29,50*</strong> e descontos de até 80% nos exames.</p>
+                                <p>Na assinatura <strong>Black</strong> Você ainda conta com o bônus de <strong>5 consultas grátis*</strong> em 2019 nas especialidades básicas.</p>
+                                <span>*Válido no DF, consulte demais estados.<br>
+                                *Marcação via central de atendimento. 0800 Doutor Hoje</span>
+                            </th>
+                            <th scope="col">
+                                <p class="nome-plano">blue</p>
+                                <p class="auxiliar">consultas a partir de:</p>
+                                <p><small>R$</small>35,50</p>
+                                <a href="#contato" class="btn btn-blue">Assinar</a>
+                            </th>
+                            <th scope="col">
+                                <p class="nome-plano">black</p>
+                                <p class="auxiliar">consultas a partir de:</p>
+                                <p><small>R$</small>69,50</p>
+                                <a href="#contato" class="btn btn-black">Assinar</a>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Desconto de até 70% em Exames</th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Desconto de até 80% em Exames</th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Programas de Promoção a Saúde e e incentivo de práticas saudáveis</th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Orientação Médica por Telefone</th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Desconto em Medicamentos</th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Consultas Especialidades Básicas e Medicina Integrada R$ 29,50
+                                <span>(Clínica Médica, Médico da Família, Cardiologia, Ginecologia, Dermatologia, Urologia, Psicologia, Acupuntura, Nutricionista e Fonoaudiologia)</span></th>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano blue contempla este benefício"></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Consultas Médicas em todas as especialidades R$ 29,50</th>
+                            <td></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Bônus de 5 consultas no 1˚ano</th>
+                            <td></td>
+                            <td><img src="/libs/home-template/img/l-pf-check.png" alt="O plano black contempla este benefício"></td>
+                        </tr>
+                        <tr>
+                            <th scope="row"></th>
+                            <td><a href="#contato" class="btn btn-blue">Assinar</a></td>
+                            <td><a href="#contato" class="btn btn-black">Assinar</a></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 <script src="/libs/comvex-template/js/jquery.min.js"></script>
 <script src="/libs/home-template/js/popper.min.js"></script>
 <script src="/libs/home-template/js/bootstrap.min.js"></script>
