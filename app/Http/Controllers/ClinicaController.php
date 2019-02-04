bf6b998816aa8cd6783e731e908720d6aa01c52b
Hugo Richard <hugorichard2010@gmail.com>
2019-02-04 14:23:29 -0200

    correcoes

--- app/Http/Controllers/ClinicaController.php
+++ app/Http/Controllers/ClinicaController.php
@@ -960,7 +960,7 @@ class ClinicaController extends Controller
 			$client = new Client(['timeout'  => 1500,]);
 
 
-			 $client->request('POST','http://192.168.1.87:8080/api/v1/gerar-plano-pagamento', [
+			 $client->request('POST',$this->url_api.'gerar-plano-pagamento', [
 				'headers' => [
 					'Authorization'     => env('TOKEN_PAGAMENTO_PRE_AUTORIZAR')
 				],
